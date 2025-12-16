a25be589e17339740956b00f07fa599c9dc1fa99
Moussabenmoussa <95001366+Moussabenmoussa@users.noreply.github.com>
2025-12-16 23:17:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 
 // ============ MIDDLEWARE ============
 app.use(cors());
+app.use(compression());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 
