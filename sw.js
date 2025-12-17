755f0a655a6b7261f7b6478d582c980a1be8a955
Moussabenmoussa <95001366+Moussabenmoussa@users.noreply.github.com>
2025-12-17 17:35:43 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = 'dzmarket-v1';
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
+});
