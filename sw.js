5ed75ab3e8a55b008df8b97a3d0471753c808bad
Moussabenmoussa <95001366+Moussabenmoussa@users.noreply.github.com>
2025-12-19 10:03:22 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE_NAME = 'dzmarket-v1';
-
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
-});
